e376e03a7b11b407c7875ea84631c0adccb8f4e2
Ankit Chaurasiya <63976851+ankitchaurasiya84@users.noreply.github.com>
2023-01-08 12:03:51 +0530

    model files added

--- src/models/note.js
+++ src/models/note.js
@@ -1,1 +1,20 @@
-gfhjkl
+const mongoose = require("mongoose");
+const NoteSchema = mongoose.Schema({
+
+    title :{
+        type : String,
+        required: true
+    },
+
+    description :{
+        type : String,
+        required: true
+    },
+    userId  :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required: true
+    }
+},{timestamps: false});
+
+module.exports = mongoose.model("Note",NoteSchema);
