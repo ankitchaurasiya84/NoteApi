1eae84ff370c274595bec1f93114caab78c30eec
Ankit Chaurasiya <63976851+ankitchaurasiya84@users.noreply.github.com>
2023-01-08 12:01:25 +0530

    auth file added

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,1 +1,22 @@
-jgg
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "NOTEAPI";
+const auth = (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+        if (token) {
+             
+            token = token.split(' ')[1];
+            let user = jwt.verify(token, SECRET_KEY);
+            req.userId = user.id;
+        }
+        else {
+            res.status(401).json({ message: "unauthorized User" });
+        }
+        next();
+    }
+    catch (error) {
+        console.log(error);
+        res.status(401).json({ message: "Catch unauthorized User" });
+    }
+}
+module.exports = auth;
