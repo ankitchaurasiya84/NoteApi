da59d5372ef0b9821d0bb72abd3cec14d289000d
Ankit Chaurasiya <63976851+ankitchaurasiya84@users.noreply.github.com>
2023-01-08 14:40:59 +0530

    added

--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -1,1 +1,10 @@
-hgjk
+const express =require("express");
+const { getNotes, createNote, deleteNote, updateNote } = require("../controllers/noteController");
+const auth = require("../middlewares/auth");
+const noteRouter=express.Router();
+
+noteRouter.get("/getNotes", auth, getNotes);
+noteRouter.post("/createNote",auth, createNote);
+noteRouter.delete("/deleteNote/:id", auth, deleteNote);
+noteRouter.put("/updateNote/:id",auth, updateNote);
+module.exports =noteRouter;
