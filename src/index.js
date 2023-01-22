480d33395fbc28d2a835df0bf5f7d0e2ec000c23
Ankit Chaurasiya <63976851+ankitchaurasiya84@users.noreply.github.com>
2023-01-22 13:20:46 +0530

    Reuploading the index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
-
 dotenv.config({path: './.env'});
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const noteRouter = require("./routes/noteRoutes");
@@ -8,17 +8,20 @@ const userRouter = require("./routes/userRoutes");
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 
+
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   console.log("http method--" + req.method + ", url-" + req.url);
   next();
 });
 app.use("/users", userRouter);
 app.use("/note", noteRouter);
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.DB_KEY)
   .then(() => {
-    app.listen(5000, () => {
-      console.log(" SERVER started port no 5000");
+    app.listen(PORT, () => {
+      console.log(" SERVER started port no : "+ PORT);
     });
   }).catch((Error) => {
     console.log(Error);
